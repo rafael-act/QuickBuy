d2daaee5a47414a6152d39deeb0ca3b503093a0f
Rafael Castro <rafael.actt@gmail.com>
2021-09-14 15:41:03 -0300

    Aula 116

--- QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "cadastro-usuario",
@@ -6,6 +8,14 @@ import { Component } from "@angular/core";
   styles: ["./cadastro.usuario.component.css"]
 })
 
-export class CadastroUsuarioComponent {
+export class CadastroUsuarioComponent implements OnInit {
+  public usuario: Usuario;
+  
+  constructor(private usuarioServico: UsuarioServico) {
 
+  }
+
+  ngOnInit(): void {
+    this.usuario = new Usuario();
+  }
 }
