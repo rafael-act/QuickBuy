880a49d1dd3de52dd726a14dd1656aa3627b4e01
Rafael Castro <rafael.actt@gmail.com>
2021-09-14 15:36:40 -0300

    Aula 115

--- QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -1,1 +1,11 @@
-﻿
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "cadastro-usuario",
+  templateUrl: "./cadastro.usuario.component.html",
+  styles: ["./cadastro.usuario.component.css"]
+})
+
+export class CadastroUsuarioComponent {
+
+}
