546b570cb0e04b4ac436c1af52c859aee6e28fd1
Rafael Castro <rafael.actt@gmail.com>
2021-09-20 13:49:27 -0300

    Aula 170 - Reativando o guarda rotas

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -39,12 +39,12 @@ import { LojaEfetivarComponent } from './loja/efetivar/loja.efetivar.component';
     TruncateModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'produto', component: ProdutoComponent },//passa por validação
+      { path: 'produto', component: ProdutoComponent, canActivate: [GuardaRotas] },//passa por validação
       { path: 'entrar', component: LoginComponent },
       { path: 'novo-usuario', component: CadastroUsuarioComponent },
       { path: 'pesquisar-produto', component: PesquisaProdutoComponent },
       { path: 'loja-produto', component: LojaProdutoComponent },
-      { path: 'loja-efetivar', component: LojaEfetivarComponent }
+      { path: 'loja-efetivar', component: LojaEfetivarComponent, canActivate: [GuardaRotas] }
     ])//sistema de rotas
   ],
   providers: [UsuarioServico, ProdutoServico],
@@ -52,4 +52,3 @@ import { LojaEfetivarComponent } from './loja/efetivar/loja.efetivar.component';
 })
 export class AppModule { }
 
-//{ path: 'produto', component: ProdutoComponent, canActivate: [GuardaRotas] },//passa por validação
